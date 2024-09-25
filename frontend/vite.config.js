eb4b37bfae49ea1f9f52d937cfa678c42f72eb98
bVisible <info@bvisible.ch>
2024-09-25 12:09:26 +0200

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -28,12 +28,15 @@ export default defineConfig({
     },
   },
   build: {
-    sourcemap: true,
+    sourcemap: false,
     outDir: `../${path.basename(path.resolve(".."))}/public/frontend`,
     emptyOutDir: true,
     target: "esnext",
     commonjsOptions: {
       include: [/tailwind.config.js/, /node_modules/],
+    },
+    rollupOptions: {
+      maxParallelFileOps: 2,
     },
   },
   optimizeDeps: {
